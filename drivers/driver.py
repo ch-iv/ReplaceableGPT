dea2138270c4f9583542744859748b40b44eafbf
ch-iv <icheppy101@gmail.com>
2023-11-09 23:04:33 -0500

    Implement a cookie cache

--- drivers/driver.py
+++ drivers/driver.py
@@ -1,7 +1,56 @@
+from __future__ import annotations
+
+import pickle
+import time
 from abc import ABC, abstractmethod
+from typing import Callable, Any, Optional
+from loguru import logger
 
 
 class Driver(ABC):
     @abstractmethod
+    def sign_in(self) -> bool:
+        pass
+
+    @property
+    @abstractmethod
+    def cookie_cache(self) -> CookieCache:
+        pass
+
+    @abstractmethod
     def apply_to(self, url: str) -> bool:
         pass
+
+
+class CookieCache:
+    def __init__(self, cookies: list[dict], last_updated: float):
+        self.cookies = cookies
+        self.last_updated = last_updated
+        self.max_age: float = 60.0 * 60
+
+    def is_valid(self) -> bool:
+        return self.cookies and self.last_updated + self.max_age > time.time()
+
+
+def load_cache(filename: str) -> Optional[CookieCache]:
+    try:
+        with open(filename, "rb") as f:
+            return pickle.load(f)
+    except Exception as e:
+        logger.warning(f"Unable to load cache\n{e}")
+        return None
+
+
+def save_cache(filename: str, cache: Optional[CookieCache]):
+    if cache:
+        with open(filename, "wb") as f:
+            pickle.dump(cache, f)
+
+
+def sign_in_required(func: Callable[[Driver, str], Any]):
+    def wrapper(driver: Driver, url: str):
+        if not driver.cookie_cache or not driver.cookie_cache.is_valid():
+            driver.sign_in()
+        return func(driver, url)
+
+    return wrapper
--- drivers/linkedin_driver.py
+++ drivers/linkedin_driver.py
@@ -1,6 +1,10 @@
+import time
+
 from .driver import Driver
+from .driver import sign_in_required, CookieCache, load_cache, save_cache
 from selenium.webdriver import Firefox
 from loguru import logger
+from typing import Optional
 
 
 def validate_linkedin_url(url: str) -> bool:
@@ -11,10 +15,15 @@ class LinkedinDriver(Driver):
     LOGIN_URL = (
         "https://www.linkedin.com/login?trk=guest_homepage-basic_nav-header-signin"
     )
+    CACHE_FILENAME = "ln_cache.pickle"
 
     def __init__(self, config: dict[str, str]):
         self.config = config
         self.browser = Firefox()
+        self._cookie_cache: Optional[CookieCache] = load_cache(self.CACHE_FILENAME)
+        if self.cookie_cache and self.cookie_cache.is_valid():
+            for cookie_dict in self.cookie_cache.cookies:
+                self.browser.add_cookie(cookie_dict)
 
     def sign_in(self) -> bool:
         try:
@@ -29,8 +38,20 @@ class LinkedinDriver(Driver):
         except Exception as e:
             logger.error(e)
             return False
+
+        self.cookie_cache = CookieCache(self.browser.get_cookies(), time.time())
+        save_cache(self.CACHE_FILENAME, self.cookie_cache)
         return True
 
+    @property
+    def cookie_cache(self) -> Optional[CookieCache]:
+        return self._cookie_cache
+
+    @cookie_cache.setter
+    def cookie_cache(self, value: CookieCache):
+        self._cookie_cache = value
+
+    @sign_in_required
     def apply_to(self, url: str) -> bool:
         if not validate_linkedin_url(url):
             logger.warning(f"Invalid LinkedIn URL {url}")
